230caff28fa30ac3f46e1d6516a0c8538618da10
MOHAN KRISHNA GANJIKUNTA <137477274+mightymistic007@users.noreply.github.com>
2025-09-02 22:33:53 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ module.exports = {
     "./public/index.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#d9fb06',
+        dark: '#1a1c1b',
+        secondary: '#3f4816',
+        muted: '#888680',
+        'dark-card': '#302f2c'
+      }
+    },
   },
   plugins: [],
 }
